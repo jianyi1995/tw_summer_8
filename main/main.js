1e543878f1a8b61187994113fb414e2c53daef4a
shaowenhan <1138500211@qq.com>
2017-05-20 01:16:25 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,31 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+	var number1 = [
+	'._.', '...', '._.', '._.', '...', 
+	'._.', '._.', '._.', '._.', '._.']
+	var number2 = [
+	'|.|', '..|', '._|', '._|', '|_|',
+	'|_.', '|_.', '..|', '|_|', '|_|']
+	var number3 = [
+	'|_|', '..|', '|_.', '._|', '..|',
+	'._|', '|_|', '..|', '|_|', '..|']
+
+    var n1 = []
+    var n2 = []
+    var n3 = []
+
+    let size = number.length
+    var tmp = 0
+
+    for (let i = 0; i < size; i++)
+    {
+    	tmp = number[i] - '0'
+    	n1.push(number1[tmp])
+    	n2.push(number2[tmp])
+    	n3.push(number3[tmp])
+    }
+    n1 = n1.join(' ')
+    n2 = n2.join(' ')
+    n3 = n3.join(' ')
+    var n = [n1, n2, n3].join('\n') + '\n'
+    return n
 };
